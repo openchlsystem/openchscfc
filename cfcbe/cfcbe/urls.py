09f82ef0b8614441a9de7e8578f0cad061d6f5ed
Nelson Adagi <thinkiqbs@gmail.com>
2025-01-06 12:39:25 +0300

    Updated files

--- cfcbe/cfcbe/urls.py
+++ cfcbe/cfcbe/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('feedback.urls')),
-    path('api/', include('ai.urls')),
+    path('api/feedback/', include('feedback.urls')),
+    path('api/ai/', include('ai.urls')),
 ]
