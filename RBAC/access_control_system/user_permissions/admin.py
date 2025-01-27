e90c697d82977dde244c375559067c6148858c5e
KabauPatrick <patrick.kabau@bitz-itc.com>
2025-01-27 06:04:20 -0500

    mine

--- RBAC/access_control_system/user_permissions/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, Role, Permission, Module
-
-# Register the models
-admin.site.register(User)
-admin.site.register(Role)
-admin.site.register(Permission)
-admin.site.register(Module)
