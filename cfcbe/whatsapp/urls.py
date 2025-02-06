32b1efdc4a1523fcf353e4a003c0f138633890ad
Franklin Karanja <franklinkaranja774@gmail.com>
2025-02-06 17:17:59 +0300

    endpoints fix

--- cfcbe/whatsapp/urls.py
+++ cfcbe/whatsapp/urls.py
@@ -21,9 +21,9 @@ urlpatterns = [
     # path('notify-user/', notify_user, name='notify_user'),
 
     # API Endpoints for WhatsApp Message, Media, and Contacts
-    path('whatsapp/messages/', WhatsAppMessageList.as_view(), name='whatsapp_messages'),
-    path('whatsapp/messages/incoming/', IncomingMessageList.as_view(), name='incoming_messages'),
-    path('whatsapp/messages/outgoing/', OutgoingMessageList.as_view(), name='outgoing_messages'),
-    path('whatsapp/media/', WhatsAppMediaList.as_view(), name='whatsapp_media'),
-    path('whatsapp/contacts/', ContactList.as_view(), name='whatsapp_contacts'),
+    path('/messages/', WhatsAppMessageList.as_view(), name='_messages'),
+    path('/messages/incoming/', IncomingMessageList.as_view(), name='incoming_messages'),
+    path('/messages/outgoing/', OutgoingMessageList.as_view(), name='outgoing_messages'),
+    path('/media/', WhatsAppMediaList.as_view(), name='_media'),
+    path('/contacts/', ContactList.as_view(), name='whatsapp_contacts'),
 ]
