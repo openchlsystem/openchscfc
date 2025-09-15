01ae76da3838f434506e507befa08369e2dc463c
marlonbino <109976447+marlonbino@users.noreply.github.com>
2025-09-15 17:31:37 +0300

    Current changes are what is needed

--- cfcbe/platform_adapters/cpims/helpline_cpims_abuse_adapter.py
+++ cfcbe/platform_adapters/cpims/helpline_cpims_abuse_adapter.py
@@ -18,7 +18,7 @@ class HelplineCPIMSAbuseAdapter(BaseAdapter):
     Adapter for the CPIMS (Child Protection Information Management System) platform integration.
     
     This adapter:
-    1. Receives case data from Helpline
+    1. Receives case data from Helpline.
     2. Transforms the data to CPIMS format
     3. Sends the case to CPIMS CRS endpoint
     4. Returns appropriate responses to the Helpline
